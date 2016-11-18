6dbc6d69777288e9cd6deb481117d6f0ecf77340
GordonCoffeeCan <gordonlee@172-16-11-145.DYNAPOOL.NYU.EDU>
2016-11-18 12:03:10 -0500

    Mac

---
+++ FPS_VR_Project/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	private CharacterController _characterController;
+
+	// Use this for initialization
+	void Start () {
+		_characterController = this.GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		Vector3 _direction;
+		_direction = Physics.gravity * 1;
+		_characterController.Move (_direction * Time.deltaTime);
+	}
+}
