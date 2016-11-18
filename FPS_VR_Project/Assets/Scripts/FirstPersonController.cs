27bc7c7ac05d5ab726ee7047d098c1a4992fb009
GordonCoffeeCan <gordonlee@Gordons-MBP.lan>
2016-11-18 16:46:47 -0500

    Mac

--- FPS_VR_Project/Assets/Scripts/FirstPersonController.cs
+++ FPS_VR_Project/Assets/Scripts/FirstPersonController.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
+	public Transform mainCamera;
+	public float speed = 5;
+
+	private Vector3 _moveDirection;
+	private float _gravity = 20;
+	private Transform _transfrom;
 
 	private CharacterController _characterController;
 
 	// Use this for initialization
 	void Start () {
+		_transfrom = this.transform;
+		_moveDirection = Vector3.zero;
 		_characterController = this.GetComponent<CharacterController> ();
 	}
 	
@@ -16,8 +24,14 @@ public class FirstPersonController : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		Vector3 _direction;
-		_direction = Physics.gravity * 1;
-		_characterController.Move (_direction * Time.deltaTime);
+		_transfrom.rotation = mainCamera.rotation;
+		if (_characterController.isGrounded) {
+			_moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			_moveDirection = _transfrom.TransformDirection (_moveDirection);
+			_moveDirection *= speed;
+		}
+		_moveDirection.y -= _gravity * Time.deltaTime;
+		_characterController.Move (_moveDirection * Time.deltaTime);
+		mainCamera.position = _transfrom.position;
 	}
 }
