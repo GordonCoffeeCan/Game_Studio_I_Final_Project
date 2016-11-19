9b439c39856ef650a924d6ca6932b7c390de52df
GordonCoffeeCan <gordonlee@Gordons-MacBook-Pro.local>
2016-11-18 20:13:54 -0500

    Mac

--- FPS_VR_Project/Assets/Scripts/FirstPersonController.cs
+++ FPS_VR_Project/Assets/Scripts/FirstPersonController.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
-	public Transform mainCamera;
 	public float speed = 5;
+	public float rotationSpeed = 8;
 
+	private Transform _mainCamera;
+	private Transform _weaponPivot;
 	private Vector3 _moveDirection;
 	private float _gravity = 20;
 	private Transform _transfrom;
@@ -13,7 +15,9 @@ public class FirstPersonController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		_mainCamera = GameObject.Find ("MainCamera").transform;
 		_transfrom = this.transform;
+		_weaponPivot = _transfrom.FindChild ("WeaponPivot");
 		_moveDirection = Vector3.zero;
 		_characterController = this.GetComponent<CharacterController> ();
 	}
@@ -24,7 +28,10 @@ public class FirstPersonController : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		_transfrom.rotation = mainCamera.rotation;
+		Quaternion _controllerTargetRotation = Quaternion.Euler (0, _mainCamera.eulerAngles.y, 0);
+		Quaternion _pivotTargetRotation = Quaternion.Euler (_mainCamera.eulerAngles.x, _mainCamera.eulerAngles.y, 0);
+		_transfrom.rotation = Quaternion.Slerp (_transfrom.rotation, _controllerTargetRotation, rotationSpeed * Time.deltaTime);
+		_weaponPivot.rotation = Quaternion.Slerp (_weaponPivot.rotation, _pivotTargetRotation, rotationSpeed * Time.deltaTime);
 		if (_characterController.isGrounded) {
 			_moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 			_moveDirection = _transfrom.TransformDirection (_moveDirection);
@@ -32,6 +39,6 @@ public class FirstPersonController : MonoBehaviour {
 		}
 		_moveDirection.y -= _gravity * Time.deltaTime;
 		_characterController.Move (_moveDirection * Time.deltaTime);
-		mainCamera.position = _transfrom.position;
+		_mainCamera.position = new Vector3(_transfrom.position.x, _transfrom.position.y + 1, _transfrom.position.z);
 	}
 }
